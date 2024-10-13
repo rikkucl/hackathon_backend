aec839fdf1dfe652b3915d7e3e69978cfe4bfae9
rikkucl <rikkun.20cl.13pado@gmail.com>
2024-10-13 14:02:49 +0900

    backend

--- backend/main.go
+++ backend/main.go
@@ -49,50 +49,133 @@ func init() {
 }
 
 // ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
+
+//差分
+//func handler(w http.ResponseWriter, r *http.Request) {
+//	switch r.Method {
+//	case http.MethodGet:
+//		// ②-1
+//		name := r.URL.Query().Get("name") // To be filled
+//		if name == "" {
+//			log.Println("fail: name is empty")
+//			w.WriteHeader(http.StatusBadRequest)
+//			return
+//		}
+//
+//		// ②-2
+//		rows, err := db.Query("SELECT id, name, age FROM user WHERE name = ?", name)
+//		if err != nil {
+//			log.Printf("fail: db.Query, %v\n", err)
+//			w.WriteHeader(http.StatusInternalServerError)
+//			return
+//		}
+//
+//		// ②-3
+//		users := make([]UserResForHTTPGet, 0)
+//		for rows.Next() {
+//			var u UserResForHTTPGet
+//			if err := rows.Scan(&u.Id, &u.Name, &u.Age); err != nil {
+//				log.Printf("fail: rows.Scan, %v\n", err)
+//
+//				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
+//					log.Printf("fail: rows.Close(), %v\n", err)
+//				}
+//				w.WriteHeader(http.StatusInternalServerError)
+//				return
+//			}
+//			users = append(users, u)
+//		}
+//
+//		// ②-4
+//		bytes, err := json.Marshal(users)
+//		if err != nil {
+//			log.Printf("fail: json.Marshal, %v\n", err)
+//			w.WriteHeader(http.StatusInternalServerError)
+//			return
+//		}
+//		w.Header().Set("Content-Type", "application/json")
+//		w.Write(bytes)
+//	case http.MethodPost:
+//		//postしたファイルを読み取るプロセス
+//		body, err := io.ReadAll(r.Body)
+//		if err != nil {
+//			w.WriteHeader(http.StatusInternalServerError)
+//		}
+//		var reqBody UserResForHTTPGet
+//		if err := json.Unmarshal(body, &reqBody); err != nil {
+//			w.WriteHeader(http.StatusInternalServerError)
+//		}
+//		defer r.Body.Close()
+//
+//		if len(reqBody.Name) > 50 || reqBody.Name == "" || reqBody.Age < 20 || reqBody.Age > 80 {
+//			w.WriteHeader(http.StatusBadRequest)
+//		}
+//
+//		//ULIDを用いてidを生成するプロセス
+//		t := time.Now()
+//		entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+//		id := ulid.MustNew(ulid.Timestamp(t), entropy)
+//
+//		//データベースに書き込む
+//		_, err2 := db.Exec("INSERT INTO user (id, name, age) VALUES (?, ?, ?)", id.String(), reqBody.Name, reqBody.Age)
+//		if err2 != nil {
+//			w.WriteHeader(http.StatusInternalServerError)
+//		}
+//
+//		//書き込みができたらステータスを変更し、idを出力
+//		w.WriteHeader(http.StatusOK)
+//		bytes, err3 := json.Marshal(responseMessage{
+//			Message: "id :" + id.String(),
+//		})
+//		if err3 != nil {
+//			w.WriteHeader(http.StatusInternalServerError)
+//			return
+//		}
+//		w.Header().Set("Content-Type", "application/json")
+//		w.Write(bytes)
+//		return
+//	default:
+//		log.Printf("fail: HTTP Method is %s\n", r.Method)
+//		w.WriteHeader(http.StatusBadRequest)
+//		return
+//	}
+//}
+
+
+//変更後
 func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 	switch r.Method {
 	case http.MethodGet:
-		// ②-1
-		name := r.URL.Query().Get("name") // To be filled
-		if name == "" {
-			log.Println("fail: name is empty")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// ②-2
-		rows, err := db.Query("SELECT id, name, age FROM user WHERE name = ?", name)
+		//Getクエリが来たらデータベースを検索
+		rows, err := db.Query("SELECT id, name, age FROM user")
 		if err != nil {
-			log.Printf("fail: db.Query, %v\n", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
-		// ②-3
-		users := make([]UserResForHTTPGet, 0)
+		//Jsonファイルにして送るプロセス
+		var items []UserResForHTTPGet
 		for rows.Next() {
 			var u UserResForHTTPGet
 			if err := rows.Scan(&u.Id, &u.Name, &u.Age); err != nil {
-				log.Printf("fail: rows.Scan, %v\n", err)
-
-				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-					log.Printf("fail: rows.Close(), %v\n", err)
-				}
+				print("error")
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			users = append(users, u)
+			items = append(items, u)
+		}
+		//Json形式にして送る
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(items); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
 		}
 
-		// ②-4
-		bytes, err := json.Marshal(users)
-		if err != nil {
-			log.Printf("fail: json.Marshal, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(bytes)
 	case http.MethodPost:
 		//postしたファイルを読み取るプロセス
 		body, err := io.ReadAll(r.Body)
@@ -138,6 +221,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
 
 func main() {
 	// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
